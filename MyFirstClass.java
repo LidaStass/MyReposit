4615211f7ee44c08ca615e4805904d0324458303
Lida Stass <lidastass@gmail.com>
2017-04-06 19:18:23 +0300

    My First Program

---
+++ MyFirstClass.java
@@ -0,0 +1,14 @@
+package cat.main.acadamy.ua;
+
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 04.04.2017.
+ */
+public class MyFirstClass {
+    public static void main(String[] arg){
+        Scanner sc=new Scanner(System.in);
+        String line=sc.nextLine();
+        System.out.print(line);
+    }
+}
